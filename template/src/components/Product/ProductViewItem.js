ea8ba1cb73c80d53f286e3e0fd117e60d3ad064a
Einar Sv <einarsve17@ru.is>
2019-03-06 13:56:02 +0000

    working on product details

--- template/src/components/Product/ProductViewItem.js
+++ template/src/components/Product/ProductViewItem.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ProductViewItemDetails from './ProductViewItemDetails'
 
 const ProductViewItem = props => {
     const { id, name, price, image } = props;
     console.log("This is the data passed on ", name)
     return (
         <div className="card text-white bg-secondary mb-3">
-            <div className="card-header">{ name }<i className="fa fa-cart-plus" style={{ float: 'right' }}></i></div>
+            <div className="card-header">{ name }<i className="fa fa-cart-plus" style={{ float: 'right' }} ></i></div>
             <div className="card-body">
-                <img src = {image} className="card-image"></img>
+    <img src = {image} className="card-image" onClick={() => <ProductViewItemDetails data = { id } />}onClick={() => console.log("Test") }></img>
                 <h4 className="card-title">Price</h4>
                 <div className="card-text">
                     <div>{price}<span> ISK</span></div>
@@ -17,11 +18,11 @@ const ProductViewItem = props => {
         </div>
     )
 }
-// ProductViewItem.propTypes = {
-//     id: PropTypes.oneOfType([ PropTypes.string, PropTypes.number]).isRequired,
-//     name: PropTypes.string.isRequired,
-//     price: PropTypes.number.isRequired,
-//     image: PropTypes.string.isRequired
-// }
+ProductViewItem.propTypes = {
+    id: PropTypes.oneOfType([ PropTypes.string, PropTypes.number]).isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired
+}
 
 export default ProductViewItem;
